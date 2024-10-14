d86e4d4baf1c662a4b22fd3f74a0d80012e22d34
Mina-Genno <minaminaponponmaru@gmail.com>
2024-10-15 03:52:45 +0900

    fix: サンプルページ変更

--- app/frontend/src/app/(route)/sample/page.tsx
+++ app/frontend/src/app/(route)/sample/page.tsx
@@ -1,22 +1,16 @@
 "use client";
 
-import httpRequest from "@/http/HttpRequest";
-import { checkLogin, handleLogout } from "@/http/api/authApi";
-import { getFromLocalStorage, saveToLocalStorage } from "@/utils/utils";
-import { Box, Button, Paper } from "@mui/material";
-import React from "react";
+import { Box,Paper } from "@mui/material";
+import React, { useState } from "react";
 
 const SamplePage = () => {
 
+  const [message, setMessage] = useState("here is sample page");
+
   const init = async () => {
-    const result = await httpRequest(
-      '/sample',
-      'test',
-      {userName: 'test', password: 'test'},
-      'GET'
-    )
-    console.log(result)
+    
   }
+
   init()
 
   return (
@@ -26,26 +20,14 @@ const SamplePage = () => {
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
+        padding: 2,
+        width: "70%",
+        margin: "100px auto 0 auto"
       }}
     >
-      <Box p={2}>
-        <h1>Sample Page</h1>
+      <Box>
+        { message }
       </Box>
-      <Button
-        onClick={() => {
-          handleLogout();
-        }}
-      >
-        ログアウト
-      </Button>
-      <Button
-        onClick={async () => {
-          const res = await checkLogin();
-          console.log(res);
-        }}
-      >
-        ログインチェック
-      </Button>
     </Paper>
   );
 };
